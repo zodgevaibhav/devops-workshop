f17d0190a6527857fd513a601b7f0169006eabd2
Vaibhav Zodge <zodgevaibhav@gmail.com>
2025-03-20 20:27:33 +0530

    learn(chef): Add example of solo config

--- chef/chef-solo/chef/solo.rb
+++ chef/chef-solo/chef/solo.rb
@@ -1,1 +1,28 @@
 cookbook_path ['./cookbooks']
+
+# Path to the local JSON file defining the run list
+json_attribs "/path/to/node.json"
+
+# Directory containing cookbooks
+#cookbook_path ["/path/to/cookbooks"]
+
+# Directory containing roles
+#role_path "/path/to/roles"
+
+# Directory for storing cache files
+#file_cache_path "/var/chef/cache"
+
+# Directory for storing backups
+#file_backup_path "/var/chef/backup"
+
+# Location of environment files
+#environment_path "/path/to/environments"
+
+# Set log level (auto, debug, info, warn, error, fatal)
+log_level :info
+
+# Define the log location (file or STDOUT)
+log_location STDOUT
+
+# Enable colored output in logs
+color true
